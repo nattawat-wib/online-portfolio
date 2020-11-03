704a52d1ffb74cff98b412e5c12f880082f68f35
nattawat.wib <nattawat.wib@gmail.com>
2020-11-03 23:53:18 +0700

    remove bg cloud

--- Portfolio/index.js
+++ Portfolio/index.js
@@ -3,22 +3,29 @@ function parallaxHeader() {
     const parallaxText = document.querySelector('.parallax__text');
     const parallaxMountain = document.querySelector('.parallax__mountain');
     const parallaxMoon = document.querySelector('.parallax__moon');
+    const parallaxCloud = document.querySelector('.parallax__cloud');
 
     let scY = window.scrollY;
-    
+
     parallaxStar.style.transform = `translate(0, ${scY * 0.1}%)`;
-    parallaxText.style.transform = `translate(0, ${scY * 0.03}%)`;
+    parallaxText.style.transform = `translate(0, ${scY * 0.1}%)`;
     parallaxMountain.style.transform = `translate(0, ${scY * 0.05}%)`;
     parallaxMoon.style.transform = `translate(${scY * 0.1}%, ${scY * -0.2}%)`
+    parallaxCloud.style.transform = `translate(0, ${scY * -0.03}%)`
 }
 
 function navbarAnimate() {
     const nav = document.querySelector('nav');
     const parallaxBg = document.querySelector('.parallax__bg');
 
-    if(pageYOffset < 200 || window.scrollY > parallaxBg.offsetHeight) {
+    if(pageYOffset < 200) {
         nav.style.opacity = '1';
+        nav.style.backgroundColor = 'transparent';
 
+    } else if(window.scrollY > parallaxBg.offsetHeight) {
+        nav.style.opacity = '1';
+        nav.style.backgroundColor = 'black';
+        
     } else {
         nav.style.opacity = '0';
     } 
@@ -27,7 +34,9 @@ function navbarAnimate() {
 function run() {
     window.addEventListener('load', () => {
         const loaderPage = document.querySelector('.loader'); 
+
         loaderPage.style.opacity = '0';
+        loaderPage.style.height = '0vh'
         setTimeout(()=>{loaderPage.style.display = 'none';} , 2000) 
     })
 
